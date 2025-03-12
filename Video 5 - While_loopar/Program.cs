acb4c972d2d85f81568c47244283c0f61ba39d59
Hampus Eriksson <hampuse1@gmail.com>
2025-03-12 17:07:18 +0100

    while

---
+++ Video 5 - While_loopar/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Video_5___While_loopar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string secret_word = "secret";
+
+            // Sätt 1
+            while (true)
+            {
+                Console.WriteLine("Guess the secret");
+                string guess = Console.ReadLine().ToLower();
+
+                if (guess == secret_word)
+                {
+                    Console.WriteLine("Correct guess!");
+                    break;
+                }
+            }
+
+            secret_word = "password";
+            string guess2 = "";
+
+            // Sätt 2
+            while (secret_word  != guess2)
+            {
+                Console.WriteLine("Guess the second secret");
+                guess2 = Console.ReadLine().ToLower();
+            }
+
+            Console.WriteLine("Correct guess!");
+        }
+    }
+}
