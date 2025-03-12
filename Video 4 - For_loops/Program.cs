76ea85b63bbc78d34612e40f7608d37884a369db
Hampus Eriksson <hampuse1@gmail.com>
2025-03-12 16:59:44 +0100

    for loopar

---
+++ Video 4 - For_loops/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Video_4___For_loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            for (int j = 7; j < 100; j+=2)
+            {
+                Console.WriteLine(j);
+            }
+
+            for (int k = 100; k >= 0; k--)
+            {
+                Console.WriteLine(k);
+            }
+
+            Console.WriteLine("Pick a startnumber");
+            int start = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Pick a stopnumber");
+            int stop = int.Parse(Console.ReadLine());
+
+            for (int whatever = start; whatever <= stop; whatever++)
+            {
+                Console.WriteLine(whatever);
+            }
+
+        }
+    }
+}
